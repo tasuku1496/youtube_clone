18d0e48c3fe9d6cff4a88a54a97ec6c4cf8d1e36
tasuku1496 <tishiguro@careerindex.co.jp>
2024-12-20 19:20:50 +0900

    feat; 画像ホストの設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["i.ytimg.com"], // 画像のホスト名を許可
+  },
 };
 
 export default nextConfig;
