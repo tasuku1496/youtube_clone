c3005dced7ccbdf349df21e0e8c7d37941cb53c3
tasuku1496 <tishiguro@careerindex.co.jp>
2024-12-20 18:10:34 +0900

    fix: APIキーは環境変数として置く

--- src/app/lib/youtube.ts
+++ src/app/lib/youtube.ts
@@ -1,4 +1,4 @@
-const YOUTUBE_API_KEY = ""; // 仮置き
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 const YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/search";
 
 export type VideoData = {
